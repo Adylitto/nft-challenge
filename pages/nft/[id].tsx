1e1a47ceddb47d543e7b36474989ca6a51f1e7b0
adylitto <adylitto@gmail.com>
2022-10-05 21:49:50 +0200

    mintpage front set up end

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function NFTdroppage() {
   return (
-    <div className='flex h-screen flex-col'>
+    <div className='flex h-screen flex-col lg:grid
+    lg:grid-cols-10'>
     {/* Left */}
-      <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
+      <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
         <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
             <div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
                 <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
@@ -17,14 +18,14 @@ function NFTdroppage() {
         </div>
       </div>
     {/* Right */} 
-      <div>
+      <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
         {/* Header */} 
-        <header>
+        <header className='flex items-center justify-between'>
           <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
-            The{''}
+            The {''}
             <span className='font-extrabold underline decoration-pink-600/50'>
               TABOOX FAM
-            </span>{''}
+            </span> {''}
             NFT Minting page
           </h1>
           <button className='rounded-full bg-rose-400 px-4 py-2 
@@ -35,11 +36,23 @@ function NFTdroppage() {
         </header>
         <hr className='my-2 border'/>
       {/* Content */} 
-      <div>
-        <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="" />
+      <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0'>
+        <img className='w-80 object-cover pb-10' 
+        src="https://links.papareact.com/bdy" 
+        alt="" />
+        <h1 className='text-3xl font-bold lg:text-5xl
+        lg:font-extrabold'>
+        </h1>
+        <p className='pt-2 text-xl text-green-500 font-bold'>
+          13 / 21 NFTs claimed so far!
+        </p>
       </div>
       {/* Mint btn */}
-       
+      <button className='mt-10 h-16 w-full rounded-full 
+      bg-red-600 text-white'>
+        Mint NFT (0.01 ETH)
+      </button>
+
       </div> 
     </div>
   )
